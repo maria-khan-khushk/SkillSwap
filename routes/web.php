7b8b0216c45659be02bf6cc3abf5bf5a3ca5d8a1
maria-khan-khushk <153493453+maria-khan-khushk@users.noreply.github.com>
2026-07-20 15:43:47 +0500

    migrations added

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\CategoryController;
 
+Route::get('/categories', [CategoryController::class, 'index']);
 Route::get('/', [HomeController::class, 'index']);
